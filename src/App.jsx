41a38f8862616b941547ef76e865a8412ab39144
Prince Gupta <102991019+pr1ncegupta@users.noreply.github.com>
2023-10-20 11:55:02 +0000

    Updated Website

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Prince Gupta",
+  title: "Developer & Content Creator",
+  email: "pr1ncegupta@icloud.com",
+  gitHub: "pr1ncegupta",
+  instagram: "pr1ncegupta",
+  linkedIn: "pr1ncegupta",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Pr1ncegupta",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/abc.png";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,29 +31,18 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "I started a page called Thrilling Veracity 🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Thrilling Veracity is a content page about Culture, Facts and History. Views Count: 15+ Million",
+    url: "https://www.instagram.com/thrillingveracity/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "I am Leading GDSC FET Jain University",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Google Developer Students Club FET Jain University aims to introduce google technologies to students",
+    url: "https://gdsc.community.dev/fet-jain-university-bengaluru/",
   },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+  
 ];
 
 const Portfolio = () => {
